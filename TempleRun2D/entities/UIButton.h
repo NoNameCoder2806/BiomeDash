20232281f8d7ce8326132859b3a9e645d8f1d81c
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-12-31 15:28:42 -0800

    Modified the UIButton class

--- TempleRun2D/entities/UIButton.h
+++ TempleRun2D/entities/UIButton.h
@@ -5,32 +5,56 @@
 
 #include <string>
 
+#include "Resources.h"
+
 // ----- UIBUTTON CLASS -----
 class UIButton
 {
 private:
+    // Button name
+    std::string name;
+
     // Button texture
-    SDL_Texture* texture = nullptr;
+    SDL_Texture* normalTexture = nullptr;
+    SDL_Texture* clickedTexture = nullptr;
 
     // Position and size
     glm::vec2 position{ 0, 0 };   // SCREEN space
     glm::vec2 size{ 0, 0 };
 
-    // Whether the object is visible
+    // Whether the object is visible and clicked
     bool visible = true;
+    bool clicked = false;
 
 public:
     UIButton() = default;
 
     UIButton(SDL_Texture* tex, glm::vec2 pos, glm::vec2 sz)
-        : texture(tex), position(pos), size(sz) {
+        : normalTexture(tex), position(pos), size(sz) {
     }
 
-    void setTexture(SDL_Texture* tex) { texture = tex; }
+    UIButton(SDL_Renderer* renderer, Resources& res, const std::string& n, glm::vec2 pos, glm::vec2 sz)
+        : name(n), position(pos), size(sz)
+    {
+        normalTexture = res.loadTexture(renderer, "assets/buttons/" + name + "_normal.png");
+        clickedTexture = res.loadTexture(renderer, "assets/buttons/" + name + "_clicked.png");
+    }
+
+    // Getters
+    std::string getName() { return name; }
+    glm::vec2 getPosition() { return position; }
+    glm::vec2 getSize() { return size; }
+    bool isVisible() { return visible; }
+    bool isClicked() { return clicked; }
+
+    // Setters
+    void setName(std::string n) { name = n; }
+    void setTexture(SDL_Texture* tex) { normalTexture = tex; }
     void setPosition(glm::vec2 pos) { position = pos; }
     void setSize(glm::vec2 sz) { size = sz; }
     void setVisible(bool v) { visible = v; }
 
+    // Helper functions
     bool isHovered(float mx, float my) const
     {
         return visible &&
@@ -38,23 +62,36 @@ public:
             my >= position.y && my <= position.y + size.y;
     }
 
-    bool isClicked(float mx, float my, bool mouseDown) const
+    void updateClicked(float mx, float my, bool mouseDown)
     {
-        return mouseDown && isHovered(mx, my);
+        clicked = mouseDown && isHovered(mx, my);
     }
 
     void render(SDL_Renderer* renderer) const
     {
-        if (!visible || !texture) return;
-
-        SDL_FRect dst
+        // If the button is not visible we don't need to draw it
+        if (!visible) 
         {
-            position.x,
-            position.y,
-            size.x,
-            size.y
-        };
+            return;
+        }
 
-        SDL_RenderTexture(renderer, texture, nullptr, &dst);
+        // Get the position of the button
+        SDL_FRect dst{ position.x, position.y, size.x, size.y };
+
+        // Store the texture to draw
+        SDL_Texture* texToDraw = nullptr;
+
+        // Choose the correct texture to draw
+        if (clicked)
+        {
+            texToDraw = clickedTexture;
+        } 
+        else
+        {
+            texToDraw = normalTexture;
+        }
+
+        // Draw the texture
+        SDL_RenderTexture(renderer, texToDraw, nullptr, &dst);
     }
 };
